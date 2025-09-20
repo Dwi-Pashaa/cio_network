51830040d321a6c17d429f89debfff82c32547eb
DwipashaDjango <dwipasha336@gmail.com>
2025-09-20 21:04:21 +0700

    Update

--- app/Http/Controllers/Pages/CustomerController.php
+++ app/Http/Controllers/Pages/CustomerController.php
@@ -36,49 +36,43 @@ class CustomerController extends Controller
         $sort = $request->sort ?? 10;
         $search = $request->search ?? null;
 
-        $customers = Customer::with(['router', 'type', 'hometown', 'rt', 'rw', 'village', 'district', 'regencie', 'vlan', 'odc', 'odp', 'olt', 'price', 'paket', 'user'])
-            ->when($search, function ($query) use ($search) {
-                $query->where('name', 'like', "%$search%")
-                    ->orWhere('email', 'like', "%$search%")
-                    ->orWhere('mac_address', 'like', "%$search%")
-                    ->orWhereHas('router', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('type', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('hometown', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('rt', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('rw', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('village', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('district', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('regencie', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('vlan', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('odc', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('odp', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    })
-                    ->orWhereHas('olt', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    });
-            })
+        $customers = Customer::with([
+            'router',
+            'type',
+            'hometown',
+            'rt',
+            'rw',
+            'village',
+            'district',
+            'regencie',
+            'vlan',
+            'odc',
+            'odp',
+            'olt',
+            'price',
+            'paket',
+            'user'
+        ])
             ->where('status', 'active')
+            ->when($search, function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('name', 'like', "%$search%")
+                        ->orWhere('email', 'like', "%$search%")
+                        ->orWhere('mac_address', 'like', "%$search%")
+                        ->orWhereHas('router', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('type', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('hometown', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('rt', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('rw', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('village', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('district', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('regencie', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('vlan', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('odc', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('odp', fn($sub) => $sub->where('name', 'like', "%$search%"))
+                        ->orWhereHas('olt', fn($sub) => $sub->where('name', 'like', "%$search%"));
+                });
+            })
             ->orderBy('id', 'DESC')
             ->paginate($sort);
 
