dcb7a8dab028de844916c07f5fcacc20622927ff
DwipashaDjango <dwipasha336@gmail.com>
2025-12-17 21:04:15 +0700

    Update

--- database/migrations/2025_12_17_195347_add_router_id_to_mac_address.php
+++ database/migrations/2025_12_17_195347_add_router_id_to_mac_address.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('mac_address', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->nullable()->after('id');
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->nullOnDelete();
+            // $table->unsignedBigInteger('user_id')->nullable()->after('id')->default(1);
+            // $table->foreign('user_id')
+            //     ->references('id')
+            //     ->on('users')
+            //     ->nullOnDelete();
 
-            $table->unsignedBigInteger('router_id')->nullable()->after('user_id')->default(1);
+            $table->unsignedBigInteger('router_id')->nullable()->after('user_id');
             $table->foreign('router_id')
                 ->references('id')
                 ->on('router_networks')
