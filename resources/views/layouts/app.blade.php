826c32641dd81b7fd5c546e36111b0d9911ef0de
DwipashaDjango <dwipasha336@gmail.com>
2026-09-10 12:55:37 +0700

    perf(views): deduplicate datatables assets and fix css cache busting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,16 +17,13 @@
     <link href="{{ asset('') }}css/demo.min.css?1738096685" rel="stylesheet" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.rtl.min.css" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/bootstrap.rtl.min.css" />
-    <style>
-        @import url('https://rsms.me/inter/inter.css');
-    </style>
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <!-- Modern Layout CSS (Header, Navbar, Tables, Settings) -->
-    <link href="{{ asset('css/modern-layout.css') }}?v={{ time() }}" rel="stylesheet" />
+    <link href="{{ asset('css/modern-layout.css') }}?v={{ file_exists(public_path('css/modern-layout.css')) ? filemtime(public_path('css/modern-layout.css')) : '1.0' }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.7/css/dataTables.dataTables.min.css">
-    <link href="https://cdn.datatables.net/v/bs5/dt-2.3.7/datatables.min.css" rel="stylesheet"
-        integrity="sha384-MnxpJFHU9dTMYXshJqBDA0d93sG4KKAFEEzynAaDqcPP7BYh11O5HxbJ6iLDWOXS" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/v/bs5/dt-2.3.7/r-2.5.0/datatables.min.css" rel="stylesheet" />
     @stack('css')
 </head>
 
@@ -93,12 +90,7 @@
     <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.15.0/echo.iife.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/alertify.min.js"></script>
-    <script src="https://cdn.datatables.net/2.3.7/js/dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/v/dt/dt-2.3.7/datatables.min.js"
-        integrity="sha384-aQ8I1X2x8U0AR8D7C4Ah0OvZlwMslQdN5YDAQBA56jXrrhcECijs/i7H+5DDrlV1" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/dt-2.3.7/r-2.5.0/datatables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
     <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
     <script>
