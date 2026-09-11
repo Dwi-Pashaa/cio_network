d713afeef5593ade3c14c4812c02ed7acf2f81a4
DwipashaDjango <dwipasha336@gmail.com>
2026-09-11 23:21:31 +0700

    feat: implement authentication logic and configure route service provider for dashboard redirection

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,8 @@ class LoginController extends Controller
         if ($user) {
             if (Hash::check($request->password, $user->password)) {
                 Auth::login($user);
-                return redirect()->route('dashboard');
+                $request->session()->regenerate();
+                return redirect()->intended(route('dashboard'));
             } else {
                 return back()->with('warning', 'Username/Password salah.');
             }
